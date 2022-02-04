b054554f078447424a97a7b94435fc0a5495eed1
dilzor28 <dilzor28@gmail.com>
2022-02-04 12:15:26 -0500

    added files

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# volatility-automation
+# Volatility Automation
+
+## This script will automate the running and processing of linux memory images
+## in order to reduce the knowledge and time required to figure out and build
+## the symbols table.
+
+### How to use
+To run this script, it requires 3 parameters, `--profile/-p`, `--image/-i`, `--vol/-v`
+The parameters are described as follows:
+- `--vol` : the absolute path to where volatility3's vol.py is location (excluding vol.py)
+- `--image` : the absolute path to where the memory image is at
+- `--profile` : `y` or `n` on if the image is the same kernel as the memory image
+
+Here is an example of what the entire process looks like:
+```
+[user@hostname ~]# python3 vol3_automation.py -f /home/user/Linux64.mem -p n -v /home/user/volatility3
+Getting profile
+Profile found: 4.14.256-197.484.amzn2.x86_64
+Added the correct profile to /root/volatility3/volatility3/symbols/
+Making directory for this run: /root/voltomation
+Getting bash command history...
+Verifying function pointers for network protocols...
+Checking processes for shared creds...
+Checking if IDT was altered...
+Getting module list to sysfs info...
+Checking system call table for hooks...
+Getting all mapped ELF files for all processes...
+Getting keyboard notifier call chain...
+Getting kernel log buffer...
+Getting loaded kernel modules...
+Getting memory maps for all processes...
+Getting memory ranges for potential code injection...
+Getting memory maps for all processes...
+Getting all processes in memory image...
+Getting tree of processes in memory image...
+Checking tty for hooks...
+Done!
+[user@hostname ~]# ll
+total 1049908
+-r--r--r--  1 root root 1073336384 Feb  2 14:54 Linux64.mem
+-rw-r--r--  1 root root       9713 Feb  4 16:58 vol3_automation.py
+drwxr-xr-x 10 root root       4096 Feb  1 16:07 volatility3
+drwxr-xr-x  2 root root        327 Feb  4 17:01 voltomation
+[user@hostname ~]# ll voltomation/
+total 492
+-rw-r--r-- 1 root root  21556 Feb  4 16:59 bash.txt
+-rw-r--r-- 1 root root    128 Feb  4 16:59 check_afinfo.txt
+-rw-r--r-- 1 root root     94 Feb  4 16:59 check_creds.txt
+-rw-r--r-- 1 root root   1033 Feb  4 16:59 check_idt.txt
+-rw-r--r-- 1 root root    107 Feb  4 16:59 check_modules.txt
+-rw-r--r-- 1 root root  38326 Feb  4 17:00 check_syscall.txt
+-rw-r--r-- 1 root root  42722 Feb  4 17:00 elfs.txt
+-rw-r--r-- 1 root root     93 Feb  4 17:00 keyboard_notifiers.txt
+-rw-r--r-- 1 root root  34185 Feb  4 17:00 kmsg.txt
+-rw-r--r-- 1 root root    719 Feb  4 17:00 lsmod.txt
+-rw-r--r-- 1 root root   9980 Feb  4 17:00 lsof.txt
+-rw-r--r-- 1 root root    146 Feb  4 17:01 malfind.txt
+-rw-r--r-- 1 root root 302120 Feb  4 17:01 maps.txt
+-rw-r--r-- 1 root root   1490 Feb  4 17:01 pslist.txt
+-rw-r--r-- 1 root root   1693 Feb  4 17:01 pstree.txt
+-rw-r--r-- 1 root root    235 Feb  4 17:01 tty_check.txt
+```
---
+++ vol3_automation.py
@@ -0,0 +1,230 @@
+import subprocess as sp, argparse, os, json, requests
+
+
+class VolatilityAutomation:
+    """
+    Class to make automation of volatility3 seamless for the user
+    They only need to point this script to the correct image and
+    directory where volatility3 resides and it will do the rest
+    Output is saved to [HOME]/voltomation.
+    Use as follows:
+    python3 vol3_automation.py -f [path_to]/Image.mem -v [path_with_vol.py]/volatility3
+    """
+    def __init__(self, vol: str, image: str, profile: str) -> None:
+        vol = vol.rstrip('/')
+        self.vol = vol
+        self.image = image
+        self.profile = profile
+
+    def find_correct_symbol_table(self) -> bool:
+        """
+        Function designed to find download and save the correct profile/symbols table for the image
+        """
+        try:
+            # Get kernel version
+            print("Getting profile")
+            if self.profile == 'y':
+                kernel_ver = sp.getoutput("uname -r")
+            elif self.profile == 'n':
+                kernel_ver = (sp.getoutput(f"python3 {self.vol}/vol.py -f {self.image} banners.Banners").split('\t')[2]).split(' ')[2]
+            else:
+                print('Error')
+                return False
+
+            print(f"Profile found: {kernel_ver}")
+
+            # Get a list of all linux profile and find ours
+            all_profiles = json.loads(requests.get(
+                'https://volatility3-symbols.s3.eu-west-1.amazonaws.com/banners.json').text)
+            linux = all_profiles.get('linux', '')
+            for url in linux.values():
+                if kernel_ver in url[0]:
+                    profile = url[0]
+
+            # Download correct symbols
+            correct_profile = requests.get(profile).text
+
+            
+            # Write symbols to correct location
+            with open(f"{self.vol}/volatility3/symbols/{profile.split('/')[-1]}", "w") as new_profile:
+                new_profile.write(correct_profile)
+            print(f"Added the correct profile to {self.vol}/volatility3/symbols/")
+            return True
+
+        except Exception as err:
+            print(f"Error: {err}")
+            return False
+
+    def run_vol_against_image(self):
+
+        """
+        Automate running all of the volatility commands against the image
+        and add a line at the tope of the file about what is in it
+        """
+
+        storage_dir = f"{os.environ['HOME']}/voltomation"
+
+        try:
+            
+            # Make folder
+            print(f"Making directory to store evidence: {storage_dir}")
+            sp.run([f"mkdir",  storage_dir])
+        
+        except Exception as err:
+            print(f"Error creating directory: {err}")
+            return False
+
+        try:
+
+            # Run commands and save to home directory under "voltomation" folder
+            print("Getting bash command history...")
+            sp.getoutput(
+                f"echo 'Recovers bash command history from memory' > {storage_dir}/bash.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.bash.Bash >> {storage_dir}/bash.txt"
+            )
+            
+            print("Verifying function pointers for network protocols...")
+            sp.getoutput(
+                f"echo 'Verifies the operation function pointers of network protocols' > {storage_dir}/check_afinfo.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.check_afinfo.Check_afinfo >> {storage_dir}/check_afinfo.txt"
+            )
+            
+            print("Checking processes for shared creds...")
+            sp.getoutput(
+                f"echo 'Checks if any processes are sharing credential structures' > {storage_dir}/check_creds.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.check_creds.Check_creds >> {storage_dir}/check_creds.txt"
+            )
+            
+            print("Checking if IDT was altered...")
+            sp.getoutput(
+                f"echo 'Checks if the IDT has been altered' > {storage_dir}/check_idt.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.check_idt.Check_idt >> {storage_dir}/check_idt.txt"
+            )
+            
+            print("Getting module list to sysfs info...")
+            sp.getoutput(
+                f"echo 'Compares module list to sysfs info, if available' > {storage_dir}/check_modules.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.check_modules.Check_modules >> {storage_dir}/check_modules.txt")
+            
+            print("Checking system call table for hooks...")
+            sp.getoutput(
+                f"echo 'Check system call table for hooks' > {storage_dir}/check_syscall.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.check_syscall.Check_syscall >> {storage_dir}/check_syscall.txt"
+                )
+            
+            print("Getting all mapped ELF files for all processes...")
+            sp.getoutput(
+                f"echo 'Lists all memory mapped ELF files for all processes' > {storage_dir}/elfs.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.elfs.Elfs >> {storage_dir}/elfs.txt"
+                )
+            
+            print("Getting keyboard notifier call chain...")
+            sp.getoutput(
+                f"echo 'Parses the keyboard notifier call chain' > {storage_dir}/keyboard_notifiers.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.keyboard_notifiers.Keyboard_notifiers >> {storage_dir}/keyboard_notifiers.txt"
+                )
+            
+            print("Getting kernel log buffer...")
+            sp.getoutput(
+                f"echo 'Kernel log buffer reader' > {storage_dir}/kmsg.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.kmsg.Kmsg >> {storage_dir}/kmsg.txt"
+                )
+            
+            print("Getting loaded kernel modules...")
+            sp.getoutput(
+                f"echo 'Lists loaded kernel modules' > {storage_dir}/lsmod.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.lsmod.Lsmod >> {storage_dir}/lsmod.txt"
+                )
+            
+            print("Getting memory maps for all processes (lsof)...")
+            sp.getoutput(
+                f"echo 'Lists all memory maps for all processes' > {storage_dir}/lsof.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.lsof.Lsof >> {storage_dir}/lsof.txt"
+                )
+            
+            print("Getting memory ranges for potential code injection...")
+            sp.getoutput(
+                f"echo 'Lists process memory ranges that potentially contain injected code' > {storage_dir}/malfind.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.malfind.Malfind >> {storage_dir}/malfind.txt"
+                )
+            
+            print("Getting memory maps for all processes (maps)...")
+            sp.getoutput(
+                f"echo 'Lists all memory maps for all processes' > {storage_dir}/maps.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.proc.Maps >> {storage_dir}/maps.txt"
+                )
+            
+            print("Getting all processes in memory image...")
+            sp.getoutput(
+                f"echo 'Lists the processes present in a particular linux memory image' > {storage_dir}/pslist.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.pslist.PsList >> {storage_dir}/pslist.txt"
+                )
+                        
+            print("Getting tree of processes in memory image...")
+            sp.getoutput(
+                f"echo 'Plugin for listing processes in a tree based on their parent process ID' > {storage_dir}/pstree.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.pstree.PsTree >> {storage_dir}/pstree.txt"
+                )
+            
+            print("Checking tty for hooks...")
+            sp.getoutput(
+                f"echo 'Checks tty devices for hooks' > {storage_dir}/tty_check.txt && "
+                f"python3 {self.vol}/vol.py -f {self.image} linux.tty_check.tty_check >> {storage_dir}/tty_check.txt"
+                )
+            
+            print('Done!')
+            return True
+
+        except Exception as err:
+            print(f"Error running volatility: {err}")
+            return False
+
+    def main(self):
+        # This triggers the profile discovery/download and subsequent processing
+        try:
+            if self.find_correct_symbol_table():
+                return self.run_vol_against_image()
+        
+        except Exception as err:
+            print(f"Error running command: {err}")
+            return False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Takes a linux memory image and automate volatility3 to run against it")
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="filename",
+        metavar="FILENAME",
+        help="Absolute path to the memory image",
+        required=True
+    )
+
+    parser.add_argument(
+        "-v",
+        "--vol",
+        dest="volpath",
+        metavar="VOLPATH",
+        help="Absolute path for directory holding volatility3's 'vol.py'",
+        required=True
+    )
+
+    parser.add_argument(
+        "-p",
+        "--profile",
+        dest="profile",
+        metavar="PROFILE",
+        help="Is the profile the same as the host OS? ('y' or 'n')",
+        choices=['y','n'],
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    VOLATILITY = args.volpath
+    source_image = args.filename
+    profile = args.profile
+    vol_aut = VolatilityAutomation(vol=VOLATILITY, image=source_image, profile=profile)
+    vol_aut.main()
